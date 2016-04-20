7409ea6ad176dd35dbefa3cf1f85d68576446467
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-20 22:13:11 +0300

    Use addAll (ArrayAdapter method) to add items to the array. Is more efficient.

--- app/src/main/java/com/example/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/ForecastFragment.java
@@ -312,5 +312,16 @@ public class ForecastFragment extends Fragment {
             // This will only happen if there was an error getting or parsing the forecast.
             return null;
         }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null){
+                mForecastAdapter.clear();
+                // In this case .addAll() is more efficient than .add() for each item separately
+                mForecastAdapter.addAll(result);
+            }
+        }
+
+
     }
 }
