29d9c950d5aff88e8f517ec406708cc2643f7d38
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-25 17:31:30 +0300

    Include a table for the locations in the db.

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -30,11 +30,11 @@ public class WeatherContract {
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
         // Human readable location string, provided by the API. Because for styling, "Mountain View is more recognizable than 94043.
-        public static final String COLLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_CITY_NAME = "city_name";
 
         // In order to uniquely pinpoint the location on the map when we launch the map intent, we store the latitude and longitude as returned by openweathermap.
-        public static final String COLLUMN_COORD_LAT = "coord_lat";
-        public static final String COLLUMN_COORD_LONG = "coord_lng";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LNG = "coord_lng";
     }
 
     /* Inner class that defines the contents of the weather table */
