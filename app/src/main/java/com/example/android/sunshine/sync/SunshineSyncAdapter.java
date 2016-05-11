c95086bf876a08e517c33b9de87a98c1077cd42f
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-11 15:27:20 +0300

    Test the scheduled sync - interval = 30s.

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncAdapter.java
@@ -45,7 +45,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     public final String LOG_TAG = SunshineSyncAdapter.class.getSimpleName();
     // Interval at which to sync with the weather, in seconds.
     // 60 seconds (1 minute) * 180 = 3 hours
-    public static final int SYNC_INTERVAL = 60 * 180;
+    public static final int SYNC_INTERVAL = 30;
     public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
 
     public SunshineSyncAdapter(Context context, boolean autoInitialize) {
