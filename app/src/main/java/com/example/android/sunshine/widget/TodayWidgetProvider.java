994fff54c415f44246c75a7f2fd7aab3d9d77479
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-20 18:17:39 +0300

    Resize widgets. Choose your size.

--- app/src/main/java/com/example/android/sunshine/widget/TodayWidgetProvider.java
+++ app/src/main/java/com/example/android/sunshine/widget/TodayWidgetProvider.java
@@ -10,7 +10,7 @@ import android.support.annotation.NonNull;
 import com.example.android.sunshine.sync.SunshineSyncAdapter;
 
 /**
- * Provider for a widget showing today's weather.
+ * Provider for a horizontally expandable widget showing today's weather.
  * <p/>
  * Delegates widget updating to {@link TodayWidgetIntentService} to ensure that
  * data retrieval is done on a background thread.
